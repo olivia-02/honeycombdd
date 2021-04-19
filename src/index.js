e3d8e3e3bf2e09e92926bcd29f2f2c416b02bab8
Olivia Robson <robson.livi@gmail.com>
2021-04-19 11:38:20 -0400

    Changing things up. Restart here!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'css/index.css'
-import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import css from "./css/index.css";
